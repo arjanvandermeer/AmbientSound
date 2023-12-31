5261c6661012ad6baddc80ba262f5992143e5263
AJ van der Meer <arjan@vdmholding.com>
2023-12-31 12:08:41 +0000

    Initial Commit

---
+++ AmbientSounds/AmbientSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbientSoundsApp.swift
+//  AmbientSounds
+//
+//  Created by Arjan van der Meer on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AmbientSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
